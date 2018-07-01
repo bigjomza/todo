62036e51e97c699939e959ee99b87b914e2aacb5
Sittichai Wongfun <bigkungzana@gmail.com>
2018-07-01 10:11:13 +0700

    Add DB and JSON Server

--- src/App.js
+++ src/App.js
@@ -7,34 +7,34 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        todos: [
-            { id: 1, name: "Raft", complete: false},
-            { id: 2, name: "The Forest", complete: false}
-        ],
-        message: ""
+      todos: [
+        { id: 1, name: "Raft", complete: false },
+        { id: 2, name: "The Forest", complete: false }
+      ],
+      message: ""
     };
 
     this.onChangMessage = this.onChangMessage.bind(this);
     this.onSubmitMessage = this.onSubmitMessage.bind(this);
   }
-  onChangMessage(e){
-    this.setState({message: e.target.value });
+  onChangMessage(e) {
+    this.setState({ message: e.target.value });
   }
 
-  onSubmitMessage(e){
+  onSubmitMessage(e) {
     //ป้องกันการรีโหลดหน้า
     e.preventDefault();
     let oldTodos = this.state.todos;
     let todoLength = this.state.todos.length;
     let lastId = this.state.todos[todoLength - 1].id;
-    let newMessage = { 
-      id: lastId + 1, name: this.state.message, 
+    let newMessage = {
+      id: lastId + 1,
+      name: this.state.message,
       complete: false
     };
     oldTodos.push(newMessage);
-    this.setState({todos: oldTodos});
+    this.setState({ todos: oldTodos });
   }
-
 
   render() {
     return (
@@ -49,11 +49,11 @@ class App extends Component {
           marginTop: 20
         }}
       >
-        <HeaderComponents/>
+        <HeaderComponents />
         <List todos={this.state.todos} />
-        <FormSubmit 
-        onChangMessage = {this.onChangMessage} 
-        onSubmitMessage = {this.onSubmitMessage}
+        <FormSubmit
+          onChangMessage={this.onChangMessage}
+          onSubmitMessage={this.onSubmitMessage}
         />
       </div>
     );
